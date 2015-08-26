59c6258b496bc1d444837247358bc30465520cb9
Domenic Bove <domenicbove@gmail.com>
2015-08-26 18:11:07 -0400

    first commit

---
+++ ba-enablment-app/kie-services/src/main/java/com/redhat/StatelessDecisionService.java
@@ -0,0 +1,9 @@
+package com.redhat;
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface StatelessDecisionService {
+
+	void runRules(Collection<Object> facts, String processId);
+}
